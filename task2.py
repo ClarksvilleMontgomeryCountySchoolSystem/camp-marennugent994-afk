6ba005b88b8f67081048cff4ac5177abb161c035
marennugent994-afk <maren.nugent994@students.cmcss.net>
2026-02-24 13:57:04 -0600

    Hi

--- task2.py
+++ task2.py
@@ -7,34 +7,41 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 
 # first
-
+print(first)
+print(type(first))
 
 # last
-
-
+print(last)
+print(type(last))
 # birth
-
+print(birth)
+print(type(birth))
+birth = int(birth)
 
 # days
-
-
+print(days)
+print(type(days))
+days = int(days)
 # p_first
-
+print(p_first)
+print(type(p_first))
 
 # p_last
-
-
+print(p_last)
+print(type(p_last))
 # phone
-
-
+print(phone)
+print(type(phone))
 # street
-
-
+print(street)
+print(type(street))
 # city
-
-
+print(city)
+print(type(city))
 # state
-
-
+print(state)
+print(type(state))
 # zip_code
-
+print(zip_code)
+print(type(zip_code))
+zip_code = str(zip_code)
